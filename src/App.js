8eb6f06955afaf8f39fa8c47baad07cdde914fea
Nolan Knill <nolanknill@gmail.com>
2022-05-27 09:51:59 -0400

    Lecture starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    page: "Home",
+  };
+
+  changePage = (pageName) => {
+    this.setState({
+      page: pageName,
+    });
+
+    window.history.pushState({}, "", `/${pageName}`);
+  };
+
+  render() {
+    return (
+      <>
+        <nav className="nav">
+          <ul className="nav__list">
+            <li className="nav__item" onClick={() => { this.changePage("Home"); }}>
+              Home
+            </li>
+            <li className="nav__item" onClick={() => { this.changePage("About"); }}>
+              About
+            </li>
+          </ul>
+        </nav>
+        {this.state.page === "Home" && <Home />}
+        {this.state.page === "About" && <About />}
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,31 @@
+import "./Home.scss";
+
+const Home = () => {
+    return (
+        <main className="home">
+            <h1>Home Page</h1>
+            <p>Today's lesson is on React Router</p>
+            <ol>
+                <li>Install react-router-dom@5.3</li>
+                <li>Import BrowserRouter, Route, and Switch into your App.js component</li>
+                <li>Provide each {`<Route>`} component with a path and component.</li>
+                <li>Multiple {`<Route>`} should be placed inside a {`<Switch>`} inside a{`<BrowserRouter>`}</li>
+            </ol>
+            <h3>Example</h3>
+            <pre>{`
+    import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+    function App(){
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home}>
+                <Route path="/about" component={About}>
+            </Switch>
+        </BrowserRouter>
+    }
+        `}</pre>
+        </main>
+    );
+}
+
+export default Home;
