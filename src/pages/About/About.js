c77d8cb1b63767aef018e26b9f96b5b3f6749b4a
Nolan Knill <nolanknill@gmail.com>
2022-05-27 10:44:38 -0400

    Initial starter code for Lecture: React Router fundamentals

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,8 +7,10 @@ export default function About() {
         <h1>About the Toronto Light Salmons</h1>
         <p>Experts consider us the greatest curling team of all time.</p>
         <p>We DO NOT approve of chocolate donut holes.</p>
-        <img className="about__class-photo" src={classImg} alt="First day of class"/>
-        <p>#SquadGoals</p>
+        <figure className="about__figure">
+          <img className="about__class-photo" src={classImg} alt="First day of class"/>
+          <figcaption>#SquadGoals</figcaption>
+        </figure>
     </main>
   )
 }
